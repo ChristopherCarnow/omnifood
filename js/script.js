9dcf2e0d589cc53eacb2ae0565c8a8f69d20c715
ChristopherCarnow <49200685+ChristopherCarnow@users.noreply.github.com>
2022-09-27 20:29:18 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -24,6 +24,16 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+// Close mobile navigation:
+const alllinks = document.querySelectorAll("a:link"); // a = anchors  :link = anchors with href only
+
+alllinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
